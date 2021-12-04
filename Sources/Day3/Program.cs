121b984f630145acc03d0f6b680183d5611afc97
WhiteBlackGoose <wbg@angouri.org>
2021-12-04 13:02:00 +0300

    Improved further

--- Sources/Day3/Program.cs
+++ Sources/Day3/Program.cs
@@ -6,13 +6,14 @@ Intel Core i7-7700HQ CPU 2.80GHz (Kaby Lake), 1 CPU, 8 logical and 4 physical co
   [Host]     : .NET 6.0.0 (6.0.21.52210), X64 RyuJIT
   DefaultJob : .NET 6.0.0 (6.0.21.52210), X64 RyuJIT
 
-|   Method |         Mean |       Error |      StdDev | Ratio |   Gen 0 | Allocated |
-|--------- |-------------:|------------:|------------:|------:|--------:|----------:|
-| PartI_v1 | 257,175.5 ns | 5,019.23 ns | 9,303.46 ns | 1.000 | 20.7520 |  65,424 B |
-| PartI_v2 |  80,643.7 ns | 1,542.59 ns | 1,894.44 ns | 0.312 |       - |         - |
-| PartI_v3 |   4,148.2 ns |   143.58 ns |   421.10 ns | 0.015 |       - |         - |
-| PartI_v4 |   3,558.7 ns |    97.39 ns |   285.62 ns | 0.014 |       - |         - |
-| PartI_v5 |     846.3 ns |    16.79 ns |    37.91 ns | 0.003 |       - |         - |
+|   Method        |         Mean |       Error |      StdDev | Ratio |   Gen 0 | Allocated |
+|---------------- |-------------:|------------:|------------:|------:|--------:|----------:|
+| PartI_v1        | 257,175.5 ns | 5,019.23 ns | 9,303.46 ns | 1.000 | 20.7520 |  65,424 B |
+| PartI_v2        |  80,643.7 ns | 1,542.59 ns | 1,894.44 ns | 0.312 |       - |         - |
+| PartI_v3        |   4,148.2 ns |   143.58 ns |   421.10 ns | 0.015 |       - |         - |
+| PartI_v4        |   3,558.7 ns |    97.39 ns |   285.62 ns | 0.014 |       - |         - |
+| PartI_v5        |     846.3 ns |    16.79 ns |    37.91 ns | 0.003 |       - |         - |
+| PartI_v5_unroll |     758.8 ns |    19.42 ns |    57.25 ns | 0.003 |       - |         - |
 
 |    Method |     Mean |     Error |    StdDev | Ratio |   Gen 0 | Allocated |
 |---------- |---------:|----------:|----------:|------:|--------:|----------:|
@@ -38,6 +39,7 @@ Console.WriteLine(b.PartI_v2());
 Console.WriteLine(b.PartI_v3());
 Console.WriteLine(b.PartI_v4());
 Console.WriteLine(b.PartI_v5());
+Console.WriteLine(b.PartI_v5_unroll());
 // Console.WriteLine(b.PartII_v1());
 #endif
 
@@ -101,7 +103,7 @@ public class ToBench
         *(ulong*)tailB += toAdd;
     }
 
-    [Benchmark(Baseline = true)]
+    //[Benchmark(Baseline = true)]
     public int PartI_v1()
     {
         var lines = input.Split("\n")[0..^1];
@@ -126,7 +128,7 @@ public class ToBench
         return co * ga;
     }
 
-    [Benchmark]
+    //[Benchmark]
     public unsafe int PartI_v2()
     {
         Span<int> counts = stackalloc int[12];
@@ -163,7 +165,7 @@ public class ToBench
         return co * ga;
     }
 
-   [Benchmark]
+    //[Benchmark]
     public unsafe int PartI_v3()
     {        
         var counts = stackalloc ushort[12];
@@ -192,7 +194,7 @@ public class ToBench
         return co * ga;
     }
 
-    [Benchmark]
+    //[Benchmark]
     public unsafe int PartI_v4()
     {
         var counts = Vector256.Create((ushort)0);
@@ -232,44 +234,103 @@ public class ToBench
         Span<Vector128<byte>> counters = stackalloc Vector128<byte>[4];
         counters.Clear();
 
-        
-         fixed (byte* c = inputBytes)
-         {
-             for (int o = 0; o < len / 255 + 1; o++)
-             {
-                 var counter = Vector128.Create((byte)0);
-                 var upper = Math.Min(len - o * 255, 255) + o * 255;
-                 var charZeros = Vector128.Create((byte)'0');
-                 
-                 var upperPtr = upper * 13 + c;
-                 //for (int i = o * 255; i < upper; i++)
-                 for (var ptr = o * 255 * 13 + c; ptr < upperPtr; ptr += 13)
-                 {
-                     var src = (Vector128<byte>*)ptr;
-                     var sub = Avx2.Subtract(*src, charZeros);
-                     counter = Avx2.Add(sub, counter);
-                     // ptr += 13;
-                 }
-                 counters[o] = counter;
-             }
-         }
+        const int BatchSize = 255;
 
-         /*
         fixed (byte* c = inputBytes)
         {
-            for (int o = 0; o < len / 255 + 1; o++)
+            for (int o = 0; o < len / BatchSize + 1; o++)
             {
-                var counter = Vector128.Create((int)0);
-                var upper = Math.Min(len - o * 255, 255) + o * 255;
-                for (int i = o * 255; i < upper; i++)
+                var counter = Vector128.Create((byte)0);
+                var upper = Math.Min(len - o * BatchSize, BatchSize) + o * BatchSize;
+                var charZeros = Vector128.Create((byte)'0');
+
+                var upperPtr = upper * 13 + c;
+                //for (int i = o * 255; i < upper; i++)
+                for (var ptr = o * BatchSize * 13 + c; ptr < upperPtr; ptr += 13)
                 {
-                    var src = (Vector128<int>*)(c + i * 13);
-                    var sub = Avx2.Subtract(*src, CharZerosByte.AsInt32());
+                    var src = (Vector128<byte>*)ptr;
+                    var sub = Avx2.Subtract(*src, charZeros);
                     counter = Avx2.Add(sub, counter);
                 }
-                counters[o] = counter.AsByte();
+                counters[o] = counter;
             }
-        }*/
+        }
+
+        /*
+       fixed (byte* c = inputBytes)
+       {
+           for (int o = 0; o < len / 255 + 1; o++)
+           {
+               var counter = Vector128.Create((int)0);
+               var upper = Math.Min(len - o * 255, 255) + o * 255;
+               for (int i = o * 255; i < upper; i++)
+               {
+                   var src = (Vector128<int>*)(c + i * 13);
+                   var sub = Avx2.Subtract(*src, CharZerosByte.AsInt32());
+                   counter = Avx2.Add(sub, counter);
+               }
+               counters[o] = counter.AsByte();
+           }
+       }*/
+
+        var ga = 0;
+        var co = 0;
+        var half = len / 2;
+        for (var i = 0; i < 12; i++)
+        {
+            ga = ga * 2;
+            co = co * 2;
+            var overall = 0;
+            foreach (var vec in counters)
+                overall += vec.GetElement(i);
+            if (overall + (input[(len - 1) * 13 + i] - '0') >= half)
+                ga++;
+            else
+                co++;
+        }
+
+        return co * ga;
+    }
+
+    [Benchmark, CAAnalyze, SkipLocalsInit]
+    public unsafe int PartI_v5_unroll()
+    {
+        var len = input.Length / 13;
+        Span<Vector128<byte>> counters = stackalloc Vector128<byte>[4];
+        counters.Clear();
+
+        const int BatchSize = 252;
+
+        fixed (byte* c = inputBytes)
+        {
+            for (int o = 0; o < len / BatchSize + 1; o++)
+            {
+                var counter = Vector128.Create((byte)0);
+                var upper = Math.Min(len - o * BatchSize, BatchSize) + o * BatchSize;
+                var charZeros = Vector128.Create((byte)'0');
+
+                var upperPtr = upper * 13 + c;
+                for (var ptr = o * BatchSize * 13 + c; ptr < upperPtr; ptr += 13)
+                {
+                    var src = (Vector128<byte>*)ptr;
+                    var sub = Avx2.Subtract(*src, charZeros);
+                    counter = Avx2.Add(sub, counter);
+                    ptr += 13;
+                    src = (Vector128<byte>*)ptr;
+                    sub = Avx2.Subtract(*src, charZeros);
+                    counter = Avx2.Add(sub, counter);
+                    ptr += 13;
+                    src = (Vector128<byte>*)ptr;
+                    sub = Avx2.Subtract(*src, charZeros);
+                    counter = Avx2.Add(sub, counter);
+                    ptr += 13;
+                    src = (Vector128<byte>*)ptr;
+                    sub = Avx2.Subtract(*src, charZeros);
+                    counter = Avx2.Add(sub, counter);
+                }
+                counters[o] = counter;
+            }
+        }
 
         var ga = 0;
         var co = 0;
